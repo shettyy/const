0eaef0a15046786d5dbb724965f3bd7f24fa0e33
Abhishek Shetty <shettyy@github.com>
2021-10-23 20:34:04 +0530

    Adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  const
+//
+//  Created by Abhishek Shetty on 23/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
