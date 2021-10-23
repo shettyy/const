6896392273446fda88c85e07bcf5ee6d8681223e
Abhishek Shetty <shettyy@github.com>
2021-10-24 00:03:00 +0530

    22.cpp

--- leetcode/easy/22.cpp
+++ leetcode/easy/22.cpp
@@ -13,31 +13,26 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* l3orig = nullptr;
-        ListNode* l3 = new ListNode();
-        while((l1!=nullptr) || (l2!=nullptr))
+        ListNode l3orig(0);
+        ListNode* l3 = &l3orig;
+        while(l1 && l2)
         {
-            //cout<<"l1"<<l1<<" l2"<<l2<<endl;
-            if (((l1!=nullptr) || (l2==nullptr)) && ((l2==nullptr) || (l1->val <= l2->val)))
+            if (l1->val <= l2->val)
             {
-                //cout<<"l1.val "<<l1->val<<endl;
                 l3->next = l1;
                 l1 = l1->next;
             }
-            else if (l2 != nullptr)
+            else
             {
-                //cout<<"l2.val "<<l2->val<<endl;
                 l3->next = l2;
                 l2 = l2->next;
             }
-            if(l3orig == nullptr)
-            {
-                l3orig = l3->next;
-            }
+
             
             l3 = l3->next;
         }
+        l3->next = l1?l1:l2;
         
-        return l3orig;
+        return l3orig.next;
     }
 };
